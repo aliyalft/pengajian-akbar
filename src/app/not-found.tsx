713d98f01dcce2e4b93c736b3a7fe96b6ecf7d38
Aliyah Rossaa <aliyahrossaa@gmail.com>
2026-09-13 18:22:12 +0700

    poster

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -109,7 +109,7 @@ export default function NotFound() {
 
   <div className="notfound-poster">
     <img
-      src="/image/poster.png"
+      src="/image/posternew.png"
       alt="Poster Pengajian Akbar MT MHABD"
     />
   </div>
